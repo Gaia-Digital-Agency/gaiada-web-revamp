3feb7175e0de3a0f29c56b558c66d19a06a15957
rizalchris <rizalchrisdev@gmail.com>
2026-04-03 10:08:16 +0800

    Feat: Add new migration files and update index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260401_075817 from './20260401_075817';
 import * as migration_20260402_094657 from './20260402_094657';
+import * as migration_20260403_020503 from './20260403_020503';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260402_094657.up,
     down: migration_20260402_094657.down,
-    name: '20260402_094657'
+    name: '20260402_094657',
+  },
+  {
+    up: migration_20260403_020503.up,
+    down: migration_20260403_020503.down,
+    name: '20260403_020503'
   },
 ];
