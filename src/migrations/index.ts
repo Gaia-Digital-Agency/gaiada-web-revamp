b106003364c14c38da694091c7c444f92295d0af
Reza Firmansyah <erzethones.inc@gmail.com>
2026-04-06 09:50:27 +0800

    feat: add hero links and CTA block support to pages schema

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260401_075817 from './20260401_075817';
 import * as migration_20260402_091926 from './20260402_091926';
+import * as migration_20260406_015001 from './20260406_015001';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260402_091926.up,
     down: migration_20260402_091926.down,
-    name: '20260402_091926'
+    name: '20260402_091926',
+  },
+  {
+    up: migration_20260406_015001.up,
+    down: migration_20260406_015001.down,
+    name: '20260406_015001'
   },
 ];
