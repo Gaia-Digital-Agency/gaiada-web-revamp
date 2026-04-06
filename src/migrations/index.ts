b12e6319f690e7763ce7794338ad1351a925475a
Reza Firmansyah <erzethones.inc@gmail.com>
2026-04-06 12:01:36 +0800

    feat: add team block to pages and image field to departments collection

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260401_075817 from './20260401_075817';
 import * as migration_20260402_091926 from './20260402_091926';
 import * as migration_20260406_015001 from './20260406_015001';
+import * as migration_20260406_040114 from './20260406_040114';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260406_015001.up,
     down: migration_20260406_015001.down,
-    name: '20260406_015001'
+    name: '20260406_015001',
+  },
+  {
+    up: migration_20260406_040114.up,
+    down: migration_20260406_040114.down,
+    name: '20260406_040114'
   },
 ];
