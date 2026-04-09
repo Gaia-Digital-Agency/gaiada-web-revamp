92bb1655b4f733f8ef090d71ea465d3c08879e5c
Reza Firmansyah <erzethones.inc@gmail.com>
2026-04-09 09:34:35 +0800

    feat: add database migration for schema updates and initialize migration tracking

---
+++ src/migrations_old/index.ts
@@ -0,0 +1,45 @@
+import * as migration_20260401_075817 from './20260401_075817';
+import * as migration_20260402_091926 from './20260402_091926';
+import * as migration_20260402_094657 from './20260402_094657';
+import * as migration_20260403_020503 from './20260403_020503';
+import * as migration_20260406_015001 from './20260406_015001';
+import * as migration_20260406_040114 from './20260406_040114';
+import * as migration_20260408_065431_check_schema from './20260408_065431_check_schema';
+
+export const migrations = [
+  {
+    up: migration_20260401_075817.up,
+    down: migration_20260401_075817.down,
+    name: '20260401_075817',
+  },
+  {
+    up: migration_20260402_091926.up,
+    down: migration_20260402_091926.down,
+    name: '20260402_091926',
+  },
+  {
+    up: migration_20260402_094657.up,
+    down: migration_20260402_094657.down,
+    name: '20260402_094657',
+  },
+  {
+    up: migration_20260403_020503.up,
+    down: migration_20260403_020503.down,
+    name: '20260403_020503',
+  },
+  {
+    up: migration_20260406_015001.up,
+    down: migration_20260406_015001.down,
+    name: '20260406_015001',
+  },
+  {
+    up: migration_20260406_040114.up,
+    down: migration_20260406_040114.down,
+    name: '20260406_040114',
+  },
+  {
+    up: migration_20260408_065431_check_schema.up,
+    down: migration_20260408_065431_check_schema.down,
+    name: '20260408_065431_check_schema'
+  },
+];
